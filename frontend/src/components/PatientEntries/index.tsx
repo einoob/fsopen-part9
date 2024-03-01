a62c9aac9675394ec2bf25e8849edf43cd56a2db
Eino Lindberg <eino.lindberg@studyo.fi>
2024-03-01 11:55:14 +0200

    Ex 9.24

--- frontend/src/components/PatientEntries/index.tsx
+++ frontend/src/components/PatientEntries/index.tsx
@@ -1,11 +1,23 @@
+import React from "react";
 import { Typography } from "@mui/material";
-import { Patient } from "../../types";
+import { Patient, Diagnose } from "../../types";
+import diagnoseService from "../../services/diagnoses";
 
 interface PatientProps {
   patient: Patient;
 }
 
 const PatientEntries: React.FC<PatientProps> = ({ patient }) => {
+  const [diagnoses, setDiagnoses] = React.useState<Diagnose[]>([]);
+
+  React.useEffect(() => {
+    const fetchDiagnoses = async () => {
+      const { data } = await diagnoseService.getAll();
+      setDiagnoses(data);
+    };
+    void fetchDiagnoses();
+  }, []);
+
   return (
     <div>
       <Typography variant="h6" style={{ marginTop: "24px" }}>
@@ -13,11 +25,17 @@ const PatientEntries: React.FC<PatientProps> = ({ patient }) => {
       </Typography>
       {patient.entries.map((entry) => (
         <div key={entry.id}>
-          <Typography><b>{new Date(entry.date).toLocaleDateString("fi-FI")}</b> {entry.description}</Typography>
-          {entry.diagnosisCodes && <Typography style={{ marginTop: "6px"}}>Diagnoses</Typography>}
-          {entry.diagnosisCodes && entry.diagnosisCodes.map((code, index) => (
-            <Typography key={index} style={{ marginLeft: "12px"}}>{code}</Typography>
-          ))}
+          <Typography>
+            <b>{new Date(entry.date).toLocaleDateString("fi-FI")}</b> {entry.description}
+          </Typography>
+          {entry.diagnosisCodes && <Typography style={{ marginTop: "6px" }}>Diagnoses</Typography>}
+          {entry.diagnosisCodes &&
+            entry.diagnosisCodes.map((code, index) => {
+              const diagnose = diagnoses.find((d) => d.code === code)
+              return <Typography key={index} style={{ marginLeft: "12px" }}>
+                {code} {diagnose?.name}
+              </Typography>;
+            })}
         </div>
       ))}
     </div>
--- frontend/src/services/patients.ts
+++ frontend/src/services/patients.ts
@@ -4,29 +4,23 @@ import { Patient, PatientFormValues } from "../types";
 import { apiBaseUrl } from "../constants";
 
 const getAll = async () => {
-  const { data } = await axios.get<Patient[]>(
-    `${apiBaseUrl}/patients`
-  );
+  const { data } = await axios.get<Patient[]>(`${apiBaseUrl}/patients`);
 
   return data;
 };
 
 const getPatient = async (id: string) => {
-  const { data } = await axios.get<Patient>(
-    `${apiBaseUrl}/patients/${id}`
-  )
+  const { data } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
   return data;
-}
+};
 const create = async (object: PatientFormValues) => {
-  const { data } = await axios.post<Patient>(
-    `${apiBaseUrl}/patients`,
-    object
-  );
+  const { data } = await axios.post<Patient>(`${apiBaseUrl}/patients`, object);
 
   return data;
 };
 
 export default {
-  getAll, create, getPatient
+  getAll,
+  create,
+  getPatient,
 };
-
